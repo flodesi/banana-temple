11e699a084c1975c86dc2b5fc8af25521401905b
flodesi <jfoldesi57@gmail.com>
2021-01-30 16:12:54 -0500

    first commit

---
+++ Assets/mainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+
+    public GameObject settingsPanel;
+
+    public void StartGame()
+    {
+    	SceneManager.LoadScene(1);
+    }
+    
+    public void OpenSettings() 
+    {
+    	settingsPanel.SetActive(true);
+    }
+    
+    public void CloseSettings() 
+    {
+    	settingsPanel.SetActive(false);
+    }
+    
+    public void ExitGame()
+    {
+    	Debug.Log("exiting game");
+    	Application.Quit();
+    }
+}
