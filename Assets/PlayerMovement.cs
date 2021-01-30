accea55aee7abdfd294f0d97d351980c7a2c9e6d
flodesi <jfoldesi57@gmail.com>
2021-01-30 18:32:12 -0500

    add controls

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 _direction;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent <Animator>();
+    }
+
+    void Update()
+    {
+        TakeInput();
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate( speed * Time.deltaTime * _direction);
+        SetAnimation(_direction);
+    }
+
+    private void TakeInput()
+    {
+        _direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            _direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _direction += Vector2.right;
+        }
+    }
+
+    private void SetAnimation(Vector2 dir)
+    {
+        _animator.SetFloat("xDir", _direction.x);
+        _animator.SetFloat("yDir", _direction.y);
+        print(_animator.GetFloat("xDir"));
+    }
+}
