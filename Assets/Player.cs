1cb13abab3a7c743b17f6e0e90707a8544a7bc79
Tony <tony.du@mail.utoronto.ca>
2021-01-30 17:26:24 -0800

    Added basic animations

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,17 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float speed;
     private Vector2 _direction;
     private Animator _animator;
     private PlayerCollisions collisions;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         _animator = GetComponent <Animator>();
         collisions = GetComponent<PlayerCollisions>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -35,25 +37,46 @@ public class PlayerMovement : MonoBehaviour
         _direction = Vector2.zero;
         if (Input.GetKey(KeyCode.W))
         {
-            _direction += Vector2.down;
+            _direction += Vector2.up;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            _direction += Vector2.right;
+            _direction += Vector2.left;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            _direction += Vector2.up;
+            _direction += Vector2.down;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            _direction += Vector2.left;
+            _direction += Vector2.right;
         }
+        // Doesn't make sense that running in a diagonal is faster than running in a cardinal direction
+        _direction.Normalize();
     }
 
     private void SetAnimation(Vector2 dir)
     {
-        _animator.SetFloat("xDir", _direction.x);
-        _animator.SetFloat("yDir", _direction.y);
+        if (Mathf.Abs(dir.x) > 0)
+        {
+            _animator.Play("MonkeyRunRight");
+            spriteRenderer.flipX = dir.x == -1;
+
+        } 
+        else if (Mathf.Abs(dir.y) > 0)
+        {
+            if (dir.y > 0)
+            {
+                _animator.Play("MonkeyRunUp");
+            }
+            else
+            {
+                _animator.Play("MonkeyRunDown");
+            }
+        }
+        else
+        {
+            _animator.Play("MonkeyDownIdle");
+        }
     }
 }
